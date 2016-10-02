4889713bba50d8fffbad028a7a17b5943137c221
Harit Vyas <hvyas@walmartlabs.com>
2016-10-01 19:11:26 -0700

    updated index.js

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 "use strict";
-let http = require('http');
-let request = require('request');
+let http = require('http')
+let request = require('request')
 
-let echoServer = http.createServer((req, res) => {
+http.createServer((req, res) => {
     console.log(`Request received at: ${req.url}`)
     //res.end('Hello World!! \n')
     //req.pipe(res)
@@ -14,18 +14,14 @@ let echoServer = http.createServer((req, res) => {
 
 let destinationUrl = '127.0.0.1:8000'
 
-let proxyServer = http.createServer((req, res) => {
+http.createServer((req, res) => {
   console.log(`Proxying request to: ${destinationUrl + req.url}`)
-  // Proxy code here
+
   let options = {
   	  headers: req.headers,
   	  url: `http://${destinationUrl}${req.url}`
   	 }
   	 request(options).pipe(res)
-  	 // Use the same HTTP verb
      options.method = req.method
-
-     // Note: streams are chainable
-     // readableStream -> writable/readableStream -> writableStream
      req.pipe(request(options)).pipe(res)
 }).listen(8001)
